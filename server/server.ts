4b42e826d727f9f460df364f3ec5dfc346c6f129
JoniShiro <88798170+JoniShiro@users.noreply.github.com>
2024-02-19 19:59:13 +0200

    create server file

--- server/server.ts
+++ server/server.ts
@@ -1,12 +1,34 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+import InfoRouter from './routes/InfoRoutes';
+import rootRouter from './routes/rootRouter';
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3004;
 
-app.get('/', express.static(path.join(__dirname, '/public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const __filename = fileURLToPath(import.meta.url);
+app.get('/', express.static(path.join(__filename, '/public')));
+
+app.use('/', rootRouter);
+app.use('/info', InfoRouter);
+
+app.all('*', (req, res) => {
+    res.status(404);
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__filename, '../..', 'views', '404.html'));
+    } else if (req.accepts('json')) {
+        res.json({ message: '404 Not Found' });
+    } else {
+        res.type('txt').send('404 Not Found');
+    }
+});
 
 app.listen(port, () => {
     console.log(`Server started at http://localhost:${port}`);
