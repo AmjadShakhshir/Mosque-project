5470e671da52cf673a0676870d4d1f48346ad5cc
JoniShiro <88798170+JoniShiro@users.noreply.github.com>
2024-02-19 20:00:57 +0200

    add the root route for the api index page

---
+++ server/routes/rootRouter.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const rootRouter = express.Router();
+const __filename = fileURLToPath(import.meta.url);
+
+rootRouter.get('^/$|/index(.html)?', (req, res) => {
+    res.sendFile(path.join(__filename, '../../..', 'views', 'index.html'));
+});
+
+export default rootRouter;
